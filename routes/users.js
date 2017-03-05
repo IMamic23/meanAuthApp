9c700e72878b345d1a9255ed6c2a0d3dc1918cc0
IMamic23 <mamic.ivica@gmail.com>
2017-03-06 00:47:45 +0100

    components and register

--- routes/users.js
+++ routes/users.js
@@ -13,6 +13,7 @@ router.post('/register', (req, res, next) => {
         username: req.body.username,
         password: req.body.password
     });
+    
     User.addUser(newUser, (err, user) => {
         if(err){
             res.json({success: false, msg:'Failed to register user'});
